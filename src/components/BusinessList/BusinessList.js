518cedf8898f86a7d399a1703d512ba436def30d
Danny Avery <danny.w.avery@gmail.com>
2017-11-22 18:00:11 -0700

    Add Yelp API integration

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import './BusinessList.css';
 import Business from '../Business/Business';
+import './BusinessList.css';
 
 class BusinessList extends React.Component {
-  render() {
-    return (
-      <div className="BusinessList">
-        {
-          this.props.businesses.map((business, index) => {
-            return <Business key={'business_'+index} business={business} />;
-          })
-        }
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="BusinessList">
+                {
+                    this.props.businesses.map(function(business, i) {
+                        return <Business key={business.id} business={business} />;
+                    })
+                }
+            </div>
+        );
+    }
 }
 
 export default BusinessList;
