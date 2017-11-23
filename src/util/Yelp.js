854be69cfbca910d334568bccf5870bf14737a25
Danny Avery <danny.w.avery@gmail.com>
2017-11-22 18:25:22 -0700

    Add a return to line #35 in Yelp.js

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -32,7 +32,7 @@ const Yelp = {
         }
       }).then(jsonResponse => {
         if (jsonResponse.businesses) {
-          jsonResponse.businesses.map(business => {
+          return jsonResponse.businesses.map(business => {
             return {
               id: business.id,
               imageSrc: business.image_url,
